6afe99a3e6433ac25571e7e7ff10c22351427b2d
Deepak Arunkumar Tiwari <dtiwari@athenahealth.com>
2020-11-07 16:21:08 +0530

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, EventEmitter, Output, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static :true}) serverContentInput : ElementRef;
+  
+  @Output() serverCreated = new EventEmitter<{serverName : string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName : string, serverContent: string}>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(nameInput : HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName : nameInput.value,
+      serverContent : this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(nameInput : HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName : nameInput.value,
+      serverContent : this.serverContentInput.nativeElement.value
+    });
+  }
+}
